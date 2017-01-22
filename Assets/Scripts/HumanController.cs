6eb113977404e2935381eec6065502e4055caa0e
Joshua Adams <n7down@gmail.com>
2017-01-22 13:17:36 -0700

    updated human controller

--- Assets/Scripts/HumanController.cs
+++ Assets/Scripts/HumanController.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class HumanController : MonoBehaviour 
 {
+	private enum State
+	{
+		MindControlled,
+		Sentient
+	};
+
+	private State currentState;
+
 	public void Start () 
 	{
-	
+		currentState = State.Sentient;
 	}
 
 	public void Update () 

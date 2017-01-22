cbfeb6c1a2bcc64cb1f528dbe7e40bbad28056b8
rachel adams <shonirue@gmail.com>
2017-01-21 22:39:49 -0700

    added physics

---
+++ Assets/Scripts/OoblogController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OoblogController : MonoBehaviour {
+	private Rigidbody2D rigidBody2D;
+	private float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+		movementSpeed = 250.0f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+		newVelocity.x = horizontalInput * movementSpeed * Time.deltaTime;
+		GetComponent<Rigidbody2D>().velocity = newVelocity;
+	
+	}
+}

0e63ab9408fbfe7c238cf2a2c6b874e164d6aad9
Joshua Adams <n7down@gmail.com>
2017-01-21 22:52:27 -0700

    added player movement

--- Assets/Scripts/OoblogController.cs
+++ Assets/Scripts/OoblogController.cs
@@ -1,23 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OoblogController : MonoBehaviour {
+public class OoblogController : MonoBehaviour 
+{
 	private Rigidbody2D rigidBody2D;
 	private float movementSpeed;
+	private bool grounded;
+	private float jumpPower;
+	private Vector3 initialScale;
 
-	// Use this for initialization
-	void Start () {
+	public void Start () 
+	{
 		rigidBody2D = GetComponent<Rigidbody2D> ();
 		movementSpeed = 250.0f;
-	
+		grounded = false;
+		jumpPower = 250.0f;
+		initialScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	public void Update () 
+	{
 		float horizontalInput = Input.GetAxis ("Horizontal");
 		Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
 		newVelocity.x = horizontalInput * movementSpeed * Time.deltaTime;
 		GetComponent<Rigidbody2D>().velocity = newVelocity;
-	
+
+		if (grounded)
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				rigidBody2D.AddForce(Vector2.up * jumpPower * Time.deltaTime, ForceMode2D.Impulse);
+				grounded = false;
+			}
+		}
+
+		if (horizontalInput != 0)
+		{
+			transform.localScale = new Vector3(Mathf.Sign(horizontalInput) * initialScale.x, 
+				initialScale.y, 
+				initialScale.z);
+		}
+	}
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+		GameObject collisionGameObject = collision.gameObject;
+		if (collisionGameObject.tag == "Platform")
+		{
+			grounded = true;
+		}
 	}
 }
